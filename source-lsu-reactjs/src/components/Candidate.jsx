e561a47ca9061c8db32b8514d23fb124b68245d0
honeyfriolo <honey.friolo@lsu.edu.ph>
2022-04-23 21:50:04 +0800

    UI

--- source-lsu-reactjs/src/components/Candidate.jsx
+++ source-lsu-reactjs/src/components/Candidate.jsx
@@ -17,13 +17,13 @@ const Candidate = ({ data, name, list, onSelectCandidate, ...props }) => {
       }}
       {...props}
     >
-      <div className="flex bg-gradient-to-r from-green-900 to-green-500 border border-black mt-5">
+      <div className="rounded-lg flex bg-gradient-to-r from-green-900 to-green-500 border border-black mt-5">
         <input type="radio" className="my-auto mr-10" />
         <div className="bg-white top-2 left-6 h-16 w-16 overflow-hidden rounded-full border-2 border-white shadow-md">
-          <img src="{data.img}" />
+          <img className =" rounded-full object-cover" src="{data.img}" />
         </div>
         <div className="text-center w-full my-auto ">{fullName} </div>
-        <span class="text-xs text-white whitespace-nowrap">{data.course_year}</span>
+        <span class="p-5 text-white whitespace-nowrap">{data.course_year}</span>
       </div>
     </div>
   );
